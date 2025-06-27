8630362646032af559ae2caf392fc224cb1c7659
Your Name <pankajshekhawat069@email.com>
2025-06-27 10:59:23 +0530

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import os
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    from .routes import user_bp
+    app.register_blueprint(user_bp, url_prefix='/api')
+    return app
---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, request, jsonify
+from .models import User
+from . import db
+
+user_bp = Blueprint('user_bp', __name__)
+
+@user_bp.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([{"id": u.id, "name": u.name, "email": u.email} for u in users])
+
+@user_bp.route('/users', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    new_user = User(name=data['name'], email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User added successfully"}), 201
