107015894ecd3559692d0d76201e25d2c46980b9
Your Name <pankajshekhawat069@email.com>
2025-06-27 15:37:40 +0530

    💡 updated project files for deployment

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,26 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-import os
+from dotenv import load_dotenv
+
+load_dotenv()  # .env file से variables लोड करें
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+
+    # Load SECRET_KEY
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "defaultsecret")
+
+    # Load MySQL URL — with pymysql
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+        "DATABASE_URL",
+        "mysql+pymysql://pankaj123:Pankaj%40123@localhost:3306/users"
+    )
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
-    from .routes import user_bp
-    app.register_blueprint(user_bp, url_prefix='/api')
+
     return app
